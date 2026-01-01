045f310bad692e6d4590e22fe066db05363b514d
Sonu64 <sonusantu64@gmail.com>
2026-01-01 16:15:31 +0530

    Added Code for Single_Responsibility_Principle

---
+++ Class_5_SOLID_SRP_OCP_LSR/srpfollowed.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+    class Product {
+        private String name;
+        private float price;
+        public Product(String name, float price) {
+            this.name = name;
+            this.price = price;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public float getPrice() {
+            return this.price;
+        }
+    }
+
+    class shoppingCart {
+        private List<Product> products = new ArrayList<>();
+        public void addProduct(Product p) {
+            products.add(p);
+        }
+        public List<Product> getProducts() {
+            return this.products;
+        }
+        public float calculateTotal() {
+            float total = 0;
+            for(Product product : products) {
+                total += product.getPrice();
+            }
+            return total;
+        }
+    }
+
+    class invoicePrinter {
+        private shoppingCart cart;
+        public invoicePrinter(shoppingCart cart){
+            this.cart = cart;
+        }
+        public void printInvoice() {
+        System.out.println("\n\n------------------------------ Invoice ------------------------------\n");
+        System.out.println("Item Name\t\t\t\t\t\t\t\tItem Price");
+        for (Product product : cart.getProducts()) {
+            System.out.print(product.getName() + "\t\t" + product.getPrice());
+            System.out.println();
+            }
+        }   
+    }
+
+    class saveToDB {
+        private shoppingCart cart;
+        public saveToDB(shoppingCart cart) {
+            this.cart = cart;
+        }
+        public void save() {
+            System.out.println("\n\nSaving Cart to Database...");
+        }
+    }
+public class srpfollowed {
+
+
+
+    public static void main(String[] args) {
+        // Creating some Products
+        Product p1 = new Product("Computer Vision for Robotics, 3rd Ed.", 1500);
+        Product p2 = new Product("Full-Stack Python Development, 1st Ed.", 1000);
+        // Creating Empty Cart and Printing Invoice
+        shoppingCart myCart = new shoppingCart();
+        invoicePrinter myInvoicePrinter = new invoicePrinter(myCart);
+        saveToDB myDBsaver = new saveToDB(myCart);
+        myInvoicePrinter.printInvoice();
+        myCart.addProduct(p1);
+        myCart.addProduct(p2);
+        myInvoicePrinter.printInvoice();
+        myDBsaver.save();
+    }
+}
